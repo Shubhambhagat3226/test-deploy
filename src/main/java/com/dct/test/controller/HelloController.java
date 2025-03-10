a585dc6251c6c18402b4ec96979fd93182c63c93
Shubhambhagat3226 <shubhambhagat3220@gmail.com>
2025-03-10 18:14:13 +0530

    hello

--- src/main/java/com/dct/test/controller/HelloController.java
+++ src/main/java/com/dct/test/controller/HelloController.java
@@ -1,12 +1,12 @@
 package com.dct.test.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String hello() {
         return "index";
     }
